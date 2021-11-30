7efc81f115b3edf01521fa8d3ed39c42176882cd
Levente Ibrányi <ibranyilevente@gmail.com>
2021-11-30 23:43:54 +0100

    kiirBor() add to main

--- src/hu/petrik/italoop/Main.java
+++ src/hu/petrik/italoop/Main.java
@@ -3,6 +3,14 @@ package hu.petrik.italoop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Bor b1 = new Bor("fehér", 1444);
+        Aszu b2 = new Aszu(1555, 21);
+        kiirBor(b1);
+        kiirBor(b2);
+
+    }
+
+    public static void kiirBor(Bor bor){
+        System.out.println(bor);
     }
 }
